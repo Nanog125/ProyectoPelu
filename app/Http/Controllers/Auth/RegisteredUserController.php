16ff01beb36b2b70498ce3b1737f391ff2d73735
Nanog125 <83281697+Nanog125@users.noreply.github.com>
2024-06-20 07:29:45 +0200

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'credit' => 5,
         ]);
 
         event(new Registered($user));

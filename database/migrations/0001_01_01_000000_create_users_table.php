a2b62bfecd6b711439a1ac863fa12caf08df3276
Nanog125 <83281697+Nanog125@users.noreply.github.com>
2024-06-20 07:29:13 +0200

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedInteger('credit')->default(5);
+            $table->unsignedInteger('credit');
             $table->rememberToken();
             $table->timestamps();
         });
